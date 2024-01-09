166d8eac214f6c139c2cab9aecd70ab09a16a1e0
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-09 16:00:19 +0530

    Server Side Rendering

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require('path')
 const app = express();
 const URL = require("./models/url");
 const { connectTomongoDB } = require("./connect");
@@ -10,9 +10,21 @@ connectTomongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDB Connected")
 );
 
+app.set("view egine","ejs");
+app.set('views', path.resolve('./views'))
+
 app.use(express.json());
+
+app.get("/test", async(req,res)=>{
+   const allUrls = await URL.find({});
+   return res.render("home")
+})
+
+
+
+
 app.use("/url", urlRoute);
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
