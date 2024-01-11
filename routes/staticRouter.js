1708de22ca8c6caf054dce1b83b92f628a0c2b9f
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-11 21:18:26 +0530

    Changes

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -9,5 +9,8 @@ router.get('/', (req,res)=>{
 router.get('/signup',(req,res) =>{
     return res.render('signup')
 } )
+router.get('/login',(req,res) =>{
+    return res.render('signup')
+} )
 
 module.exports = router;
