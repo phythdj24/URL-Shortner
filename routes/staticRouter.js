992055b78c4a43c39fb2ed49609b75d06a7267ce
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-12 16:00:00 +0530

    Chnages

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -9,8 +9,6 @@ router.get('/', (req,res)=>{
 router.get('/signup',(req,res) =>{
     return res.render('signup')
 } )
-router.get('/login',(req,res) =>{
-    return res.render('signup')
-} )
+
 
 module.exports = router;
